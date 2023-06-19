37738d078d89b1355ee1227531b1886300d3947c
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-06-19 16:09:32 -0300

    12

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -8,7 +8,7 @@ const Formulario = () => {
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <CampoTexto label="Imagem2" placeholder="Digite o endereço da imagem" />
             </form>
         </section>
     )
