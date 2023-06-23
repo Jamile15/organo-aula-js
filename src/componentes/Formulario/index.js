f8ed63f0d89c25e4f32afaf23b85c353ff0214b4
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-06-23 10:38:10 -0300

    Botão criar card usando children

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from '../Botão'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
@@ -21,6 +22,9 @@ const Formulario = () => {
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
                 <ListaSuspensa label="Time"  item={time}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
