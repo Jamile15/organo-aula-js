3ca37922311c24ab5725306d09401d0da1a2e33b
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-06-26 14:00:03 -0300

    teste console pegando valor do imput

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,14 +2,27 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
-    const placeholderModificada = `${props.placeholder}...` 
+    const placeholderModificada = `${props.placeholder}...`
+    
+
+    let valor = 'jamile'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
 
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatoria} placeholder={placeholderModificada}/>
+            <input
+                value={valor}
+                onChange={aoDigitado}
+                required={props.obrigatoria}
+                placeholder={placeholderModificada}
+            />
         </div>
     )
 }
