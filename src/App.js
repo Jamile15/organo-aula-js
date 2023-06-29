5b7f33e1ac3aeccd78a6ac6b6890d52074455f03
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-06-29 09:41:17 -0300

    fazendo cada section ter uma cor puxando com o map direto pela const times

--- src/App.js
+++ src/App.js
@@ -4,49 +4,49 @@ import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 
 function App() {
-    const times = [
-      {
-        nome: 'Programação',
-        corPrimaria: '#57C278',
-        corSecundaria: '#D9F7E9'
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
 
-      },
-      {
-        nome: 'Front-End',
-        corPrimaria: '#82CFFA',
-        corSecundaria: '#E8F8FF',
     },
     {
-        nome: 'Data Sciense',
-        corPrimaria: '#A6D157',
-        corSecundaria: '#F0F8E2',
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
     },
     {
-        nome: 'Devops',
-        corPrimaria: '#E06B69',
-        corSecundaria: '#FDE7E8'
+      nome: 'Data Sciense',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
     },
     {
-        nome: 'UX e Design',
-        corPrimaria: '#D86EBF',
-        corSecundaria: '#FAE5F5',
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
     },
     {
-        nome: 'Mobile',
-        corPrimaria: '#FEBA05',
-        corSecundaria: '#FFF5D9',
+      nome: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE5F5',
     },
     {
-        nome: 'Inovação e Gestão',
-        corPrimaria: '#FF8A29',
-        corSecundaria: '#FFEEDF',
+      nome: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
     }
 
-    ]
+  ]
 
   const [colaboradores, setColabotadores] = useState([]);
 
-  const aoNovoColaboradorAdicionado = (colaborador) =>{
+  const aoNovoColaboradorAdicionado = (colaborador) => {
     console.log(colaborador)
     setColabotadores([...colaboradores, colaborador])
 
@@ -55,8 +55,13 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
-    
-      {times.map( time =>  <Time nome={time.nome}/> )}
+
+      {times.map(time => <Time
+        key={time.nome}
+        nome={time.nome}
+        corPrimaria={time.corPrimaria}
+        corSecundaria={time.corSecundaria}
+      />)}
 
 
 
