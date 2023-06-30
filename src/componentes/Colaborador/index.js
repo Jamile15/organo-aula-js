33a290e4192f82edc2725e7f3be992308ce12050
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-06-30 10:38:55 -0300

    adicionando o componente base dos colaboradores

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return(
+        <div>
+            <div>
+                <img src='http://github.com/jamile15.png' alt='jamile vieira'/>
+            </div>
+            <div>
+                <h4>jamile</h4>
+                <h5>dev</h5>
+            </div>
+        </div>
+
+    )
+}
+export default Colaborador
