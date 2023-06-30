8646cf94cb37f4ccea3f8dd48a8f48808825e28a
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-06-30 10:43:56 -0300

    adicionando um estilo basico nos colaboradorees

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -2,11 +2,11 @@ import './Colaborador.css'
 
 const Colaborador = () => {
     return(
-        <div>
-            <div>
+        <div className='colaborador'>
+            <div className='cabecalho'>
                 <img src='http://github.com/jamile15.png' alt='jamile vieira'/>
             </div>
-            <div>
+            <div className='rodape'>
                 <h4>jamile</h4>
                 <h5>dev</h5>
             </div>
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,11 +2,15 @@ import Colaborador from '../Colaborador';
 import './Time.css'
 
 const Time = (props) => {
-    const css = {backgroundColor: props.corSecundaria}
-    return(
+    const css = { backgroundColor: props.corSecundaria }
+    return (
         <section className='time' style={css}>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-            <Colaborador/>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <Colaborador />
+            <Colaborador />
+
+            <Colaborador />
+
         </section>
     )
 }
